87e91646ef753a654193a4759a9440aa8b3ba016
Dom <ducommun.dom@gmail.com>
2017-01-25 21:14:49 +0100

    Visual update and rework on forms

--- snapventure-backend/snapventure/forms.py
+++ snapventure-backend/snapventure/forms.py
@@ -3,7 +3,7 @@ from .models import Profile, Journey, Inscription, Type, Step, Scan
 from django.contrib.auth.models import User
 from django.forms import modelformset_factory
 from tinymce.widgets import TinyMCE
-
+from django.forms import ModelForm, TextInput
 
 class UserForm(forms.ModelForm):
     class Meta:
@@ -20,7 +20,7 @@ class JourneyForm(forms.ModelForm):
     class Meta:
         model = Journey
         fields = ['name', 'description', 'img_description', 'img_ambiance', 'start_time', 'end_time', 'private', 'active']
-        widgets = {'description': TinyMCE(attrs={'cols': 80, 'rows': 30})}
+        widgets = {'name': TextInput(attrs={'placeholder':'Enter title of your journey here'}),}
 
 
 class StepForm(forms.ModelForm):
